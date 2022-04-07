0287111c1212c6485f377b43a9f3385d396ee20c
İsmail Basık <87307720+ismailbask@users.noreply.github.com>
2022-04-07 16:23:36 +0300

    database connection and data insertion operations

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+const Post = require('./models/Posts');
+
+const app = express();
+
+//connect db
+mongoose.connect('mongodb://localhost/cleanblog-test-db');
 
 //Middlewares
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}))
 
-app.get('/', (req, res) => {
-    // res.sendFile(path.resolve(__dirname, 'views/index.html'));
-    res.render('index')
+app.get('/', async (req, res) => {
+    const posts = await Post.find({});
+    res.render('index', {
+        posts
+    })
 });
 app.get('/about', (req, res) => {
     // res.sendFile(path.resolve(__dirname, 'views/index.html'));
@@ -18,6 +30,11 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
     // res.sendFile(path.resolve(__dirname, 'views/index.html'));
     res.render('add_post')
+});
+app.post('/posts', async (req, res) => {
+    // console.log(req.body);
+    await Post.create(req.body);
+    res.redirect('/');
 });
 const PORT = 5000;
 app.listen(PORT, () => {
